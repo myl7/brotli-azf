ba7f156ed1220b3ae6038d2b270dae072f9e1ccd
myl7 <myl@myl.moe>
2021-10-18 01:53:09 +0800

    feat: Add basic handling login

--- enc/handler.go
+++ enc/handler.go
@@ -1,5 +1,50 @@
 package enc
 
-import "net/http"
+import (
+	"fmt"
+	"log"
+	"mime/multipart"
+	"net/http"
+)
 
-func Handle(w http.ResponseWriter, req *http.Request) {}
+func Handle(w http.ResponseWriter, req *http.Request) {
+	c, err := NewConfig()
+	if err != nil {
+		responseErr(w, 500, "App invalid config")
+		return
+	}
+
+	err = req.ParseMultipartForm(21 * 1024 * 1024)
+	if err != nil {
+		responseErr(w, 400, "Parsing multipart form failed")
+		return
+	}
+
+	form := req.MultipartForm
+	mode := ""
+	modes, ok := form.Value["mode"]
+	if ok {
+		mode = modes[0]
+	} else {
+		mode = "once"
+	}
+
+	switch mode {
+	case "once":
+		handleOnce(w, form, c)
+	case "chunked":
+		handleChunked(w, form, c)
+	default:
+		responseErr(w, 400, "Invalid mode param")
+	}
+}
+
+func handleOnce(w http.ResponseWriter, form *multipart.Form, c Config) {}
+
+func handleChunked(w http.ResponseWriter, form *multipart.Form, c Config) {}
+
+func responseErr(w http.ResponseWriter, status int, reason string) {
+	w.WriteHeader(status)
+	_, _ = fmt.Fprintln(w, reason)
+	log.Printf("Err: status = %d, reason = %s", status, reason)
+}
